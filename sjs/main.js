957f3ec4d192e38e0bc87ea2f83d7c80fd04e794
James Tomasino <james@tomasino.org>
2013-01-31 14:39:08 -0500

    adding styles and js

--- sjs/main.js
+++ sjs/main.js
@@ -1,2 +1,7 @@
 //= require lib/jquery-1.8.2
-//= require lib/l10n
+//= require lib/redactor.min
+//= require lib/store
+
+$(document).ready( function(){
+	$('#redactor_content').redactor({focus: true});
+});
