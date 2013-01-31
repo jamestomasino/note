784e7434eb41b2e802b4bf2bf67b27a6acd59aa3
James Tomasino <james@tomasino.org>
2013-01-31 14:53:14 -0500

    store js working

--- sjs/main.js
+++ sjs/main.js
@@ -3,5 +3,14 @@
 //= require lib/store
 
 $(document).ready( function(){
-	$('#redactor_content').redactor({focus: true});
+	$('#redactor_content').redactor({focus: true, keyupCallback: keyUpHandler});
+	var html = store.get( 'localnote' );
+	if (html) {
+		$('#redactor_content').setCode(html);
+	}
 });
+
+function keyUpHandler ( obj, evt ) {
+	var html = $('#redactor_content').getCode();
+	store.set ( 'localnote', html );
+}
